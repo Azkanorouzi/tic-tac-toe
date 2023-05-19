642f8723a136936a818df3e4a2ba6cbaec45158d
Azka norouzi <azka@azkas-MacBook-Air.local>
2023-05-19 10:36:45 +0330

    init options-container

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
-// We are gonna be using module pattern for gameboard because this is only an object which controls the flow of our application in general and that's really all it does
-const displayController = (function () {
-  // Event listeners
+const elements = (function () {
+  // returns a DOM element
+  _getElement = (selector) => document.querySelector(selector)
+  const BOARD = _getElement('.board')
+  return {}
 })()
+const displayController = (function () {})()
 const gameBoard = (function () {
   const playerX = player('X')
   const playerO = player('O')
@@ -10,5 +13,9 @@ const gameBoard = (function () {
 const player = function (sign) {
   const sign = sign
   let roundWon = 0
-  return { sign, roundWon }
+  // Returns true if it's the player's turn
+  function isTurn(num, condition) {
+    return condition(num)
+  }
+  return { sign, roundWon, isTurn }
 }
